0a05a2f54b58240d4ee19b0f5cad77a2940c48e9
Tanay Sinha <tanaysinha@Tanays-MacBook-Air.local>
2026-08-02 04:07:52 +0530

    render database

--- db.js
+++ db.js
@@ -1,11 +1,22 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//     host: "localhost",
+//     port: 5432,
+//     user: "tanaysinha",
+//     database: "smart_failure_detection",
+    
+// });
+
+// module.exports = pool;
+
+
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: "localhost",
-    port: 5432,
-    user: "tanaysinha",
-    database: "smart_failure_detection",
-    
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
